af55ec318d7df96ecde8105a9b5930c86d081787
Olukolade <olukoladekaka@gmail.com>
2026-03-26 13:50:36 -0500

    Updated event listener

--- asset/script/script.js
+++ asset/script/script.js
@@ -3,7 +3,7 @@ import { Subscriber } from "./subscriber.js";
 import { createPost } from "./post.js";
 
 const textArea = document.getElementById("text");
-const imageUpload = document.getElementById("imgUpload");
+const imgUpload = document.getElementById("imgUpload");
 const postBtn = document.getElementById("postBtn");
 const postsContainer = document.getElementById("postsContainer");
 const accountBtn = document.getElementById("profileBtn");
@@ -11,3 +11,20 @@ const modal = document.getElementById("modal");
 const closeModal = document.getElementById("closeModal");
 const accountInfo = document.getElementById("accountInfo");
 const fileName = document.getElementById("fileName");
+
+
+imgUpload.addEventListener("change", () => {
+    if (imgUpload.files.length > 0) {
+        fileName.textContent = imageUpload.files[0].name;
+    }
+});
+
+const user = new Subscriber(
+    "1",
+    "Olukolade Kaka",
+    "Kay",
+    "olukoladekaka@gmail.com",
+    ["Student","MITT"],
+    ["Product Designer","Ecommerce Website"],
+    true
+);
