89f33ad7d662944ad11ddc1695e68e972bec49c3
Olukolade <olukoladekaka@gmail.com>
2026-03-26 13:42:19 -0500

    ...getElementById

--- asset/script/script.js
+++ asset/script/script.js
@@ -1,3 +1,13 @@
 'use strict';
 import { Subscriber } from "./subscriber.js";
 import { createPost } from "./post.js";
+
+const textArea = document.getElementById("text");
+const imageUpload = document.getElementById("imgUpload");
+const postBtn = document.getElementById("postBtn");
+const postsContainer = document.getElementById("postsContainer");
+const accountBtn = document.getElementById("profileBtn");
+const modal = document.getElementById("modal");
+const closeModal = document.getElementById("closeModal");
+const accountInfo = document.getElementById("accountInfo");
+const fileName = document.getElementById("fileName");
